31787f293ef7d7f7b04ba081a41a13a57bbd9cb4
Patrick McLain <patrick.mclain@ab-inbev.com>
2022-04-13 16:57:27 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    newrelic = {
+      source = "newrelic/newrelic"
+    }
+  }
+}
+
+provider "newrelic" {}
+
+data "newrelic_entity" "meal_planner" {
+  name   = "meal-planner"
+  domain = "BROWSER"
+}
+
+resource "newrelic_one_dashboard" "meal_planner" {
+  name = "Example"
+
+  page {
+    name = "Example Dashboard"
+
+    widget_billboard {
+      column = 1
+      row    = 1
+      height = 2
+      width  = 2
+      title  = "Core web vitals - LCP"
+      nrql_query {
+        query = <<NRQL
+          SELECT percentile(largestContentfulPaint, 75)
+          FROM PageViewTiming
+          WHERE appName = 'meal-planner'
+        NRQL
+      }
+    }
+  }
+}
